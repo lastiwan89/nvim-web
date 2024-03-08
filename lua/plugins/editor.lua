fd0f0204cc73d0b7283ecf31266b559fa0f8a148
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-03-08 10:46:46 +0900

    lsp to editor nvimtree focus

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -32,4 +32,14 @@ return {
       }
     end,
   },
+  {
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+      "nvim-tree/nvim-web-devicons", -- optional
+    },
+    config = function()
+      require("lspsaga").setup({})
+    end,
+  },
 }
--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -57,5 +57,6 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    keymap.set("n", "<leader>eF", "<cmd>NvimTreeFocus<CR>", { desc = "Toggle file explorer focus" }) -- toggle file explorer focus
   end,
 }
