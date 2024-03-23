f5f4aa7e48e6f30b605ec116bd1d54fc3277d73c
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-03-24 08:42:33 +0900

    add autopair plugin

--- lua/chillrains/plugins/init.lua
+++ lua/chillrains/plugins/init.lua
@@ -1,6 +1,14 @@
 return {
-  {
-    "nvim-lua/plenary.nvim",
-    name = "plenary"
-  },
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equalent to setup({}) function
+    }
 }
